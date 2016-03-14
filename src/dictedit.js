99223ca95f8fae1935ef1df21a40658081403cff
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-03-13 23:37:31 -0700

    ac

--- src/dictedit.js
+++ src/dictedit.js
@@ -1,5 +1,14 @@
 var dictedit = {
-  init: function(opts){
+  init: function(id, opts){
     opts = opts || {};
+    var ta = document.getElementById(id);
+    var newNode = document.createElement("div");
+    newNode.className = "dictedit-wrapper";
+    ta.parentNode.insertBefore(newNode,ta);
+    ta.style.display = "none";
+    var keys = Object.keys(opts);
+    for(var i = 0; i < keys.length; i++){
+      ta.dataset["dictedit_" + keys[i]] = opts[keys[i]];
+    }
   }
 }
