cb0e7f942eed2993efd886cc1df3ffacaaed2740
ani6gup <ani6gup@gmail.com>
2016-04-11 11:08:33 -0700

    Added plugin code

--- src/dictedit.js
+++ src/dictedit.js
@@ -92,5 +92,59 @@ var dictedit = {
   },
   prompt: function(c){
     return prompt(c);
+  },
+  plugin: function() {
+    (function($){
+        $.dictEditPlugin = function(el, radius, options){
+            // To avoid scope issues, use 'base' instead of 'this'
+            // to reference this class from internal events and functions.
+            var base = this;
+            
+            // Access to jQuery and DOM versions of element
+            base.$el = $(el);
+            base.el = el;
+            
+            // Add a reverse reference to the DOM object
+            base.$el.data("dictEditPlugin", base);
+            
+            base.init = function(){
+                if( typeof( radius ) === "undefined" || radius === null ) radius = "20px";
+                
+                base.radius = radius;
+                
+                base.options = $.extend({},$.dictEditPlugin.defaultOptions, options);
+                
+                // Put your initialization code here
+            };
+            
+            // Sample Function, Uncomment to use
+             /*base.functionName = function(paramaters){
+                 console.log("Is this doing anything");
+             };*/
+            
+            // Run initializer
+            base.init();
+            //base.functionName();
+        };
+        
+        $.dictEditPlugin.defaultOptions = {
+            radius: "20px"
+        };
+        
+        $.fn.dictEditPlugin = function(radius, options){
+            return this.each(function(){
+                (new $.dictEditPlugin(this, radius, options));
+
+           // HAVE YOUR PLUGIN DO STUFF HERE
+
+          
+                console.log("Is this doing anything");
+           // END DOING STUFF
+
+            });
+        };
+    
+        })(jQuery);
   }
+
 }
